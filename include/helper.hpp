79c229da690e1140ac290e4c834a7da68aa93bba
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2023-04-11 20:21:32 -0500

    T1x: Change offsets to masks

--- include/helper.hpp
+++ include/helper.hpp
@@ -169,6 +169,52 @@ namespace Memory
         return nullptr;
     }
 
+    std::uint8_t* PatternScanW(void* module, const wchar_t* signature)
+    {
+        static auto pattern_to_byte = [](const wchar_t* pattern) {
+            auto bytes = std::vector<int>{};
+            auto start = const_cast<wchar_t*>(pattern);
+            auto end = const_cast<wchar_t*>(pattern) + wcslen(pattern);
+
+            for (auto current = start; current < end; ++current) {
+                if (*current == L'?') {
+                    ++current;
+                    if (*current == L'?')
+                        ++current;
+                    bytes.push_back(-1);
+                }
+                else {
+                    bytes.push_back(wcstoul(current, &current, 16));
+                }
+            }
+            return bytes;
+        };
+
+        auto dosHeader = (PIMAGE_DOS_HEADER)module;
+        auto ntHeaders = (PIMAGE_NT_HEADERS)((std::uint8_t*)module + dosHeader->e_lfanew);
+
+        auto sizeOfImage = ntHeaders->OptionalHeader.SizeOfImage;
+        auto patternBytes = pattern_to_byte(signature);
+        auto scanBytes = reinterpret_cast<std::uint8_t*>(module);
+
+        auto s = patternBytes.size();
+        auto d = patternBytes.data();
+
+        for (auto i = 0ul; i < sizeOfImage - s; ++i) {
+            bool found = true;
+            for (auto j = 0ul; j < s; ++j) {
+                if (scanBytes[i + j] != d[j] && d[j] != -1) {
+                    found = false;
+                    break;
+                }
+            }
+            if (found) {
+                return &scanBytes[i];
+            }
+        }
+        return nullptr;
+    }
+
     std::string GetVersionString()
     {
         auto hInst = GetModuleHandle(NULL);
--- source/T1X.DebugFeatures/dllmain.cpp
+++ source/T1X.DebugFeatures/dllmain.cpp
@@ -41,7 +41,7 @@ const wchar_t* GetBoolStr(bool input_bool)
     return input_bool ? L"true" : L"false";
 }
 
-#define LOG(fmt, ...) file_log(L"%-24s:%u " fmt "\n", __FUNCTIONW__, __LINE__, __VA_ARGS__);
+#define LOG(fmt, ...) file_log(L"%-24s:%u " fmt, __FUNCTIONW__, __LINE__, __VA_ARGS__);
 
 void LoggingInit(void)
 {
@@ -51,7 +51,7 @@ void LoggingInit(void)
         bLoggingEnabled = true;
         std::locale::global(std::locale("en_US.utf8"));
         current_time = std::time(nullptr);
-        LOG(L"Log file opened at " _PROJECT_LOG_PATH);
+        LOG(L"Log file opened at " _PROJECT_LOG_PATH "\n");
     }
     else
     {
@@ -73,9 +73,9 @@ void ReadConfig(void)
     std::wstring exePathWString(exePath);
     std::wstring wsGameName = Memory::GetVersionProductName();
 
-    LOG(_PROJECT_NAME " Built: " __TIME__ " @ " __DATE__);
-    LOG(L"Game Name: %s", wsGameName.c_str());
-    LOG(L"Game Path: %s", exePathWString.c_str());
+    LOG(_PROJECT_NAME " Built: " __TIME__ " @ " __DATE__ "\n");
+    LOG(L"Game Name: %s\n", wsGameName.c_str());
+    LOG(L"Game Path: %s\n", exePathWString.c_str());
 
     // Initialize config
     std::wstring config_path = _PROJECT_NAME L".ini";
@@ -83,7 +83,7 @@ void ReadConfig(void)
     if (!iniFile)
     {
         // no ini, lets generate one.
-        LOG(L"Failed to load config file.");
+        LOG(L"Failed to load config file.\n");
         std::wstring ini_defaults = L"[Settings]\n"
                                     wstr(bDebugMenu)" = true\n"
                                     wstr(bShowDebugConsole)" = false\n"
@@ -96,7 +96,7 @@ void ReadConfig(void)
         bShowDebugConsole = false;
         bExtendedDebugMenu = true;
         fDebugMenuSize = 0.6;
-        LOG(L"Created default config file.");
+        LOG(L"Created default config file.\n");
     }
     else
     {
@@ -108,23 +108,25 @@ void ReadConfig(void)
     }
 
     // Log config parse
-    LOG(L"%s: %s (%i)", wstr(bDebugMenu), GetBoolStr(bDebugMenu) , bDebugMenu);
-    LOG(L"%s: %s (%i)", wstr(bShowDebugConsole), GetBoolStr(bShowDebugConsole), bShowDebugConsole);
-    LOG(L"%s: (%f)",    wstr(fDebugMenuSize), fDebugMenuSize);
-    LOG(L"%s: %s (%i)", wstr(bExtendedDebugMenu), GetBoolStr(bExtendedDebugMenu), bExtendedDebugMenu);
+    LOG(L"%s: %s (%i)\n", wstr(bDebugMenu), GetBoolStr(bDebugMenu) , bDebugMenu);
+    LOG(L"%s: %s (%i)\n", wstr(bShowDebugConsole), GetBoolStr(bShowDebugConsole), bShowDebugConsole);
+    LOG(L"%s: (%f)\n",    wstr(fDebugMenuSize), fDebugMenuSize);
+    LOG(L"%s: %s (%i)\n", wstr(bExtendedDebugMenu), GetBoolStr(bExtendedDebugMenu), bExtendedDebugMenu);
 }
 
-void ShowPatchInfo(size_t Hook_Length, size_t Patch_Size, uint64_t Patch_Addr, const wchar_t* Patch_Name)
+void ShowPatchInfo(size_t Patch_Size, uint64_t Patch_Addr, const wchar_t* Patch_Name, uint64_t Patch_Function_Target)
 {
-    LOG(L"Patch Name: %s", Patch_Name);
-    if (Hook_Length)
-        LOG(L"Hook length: %u bytes", Hook_Length);
-    LOG(L"Patch length: %llu bytes", Patch_Size);
-    LOG(L"Patch address: 0x%016llx", Patch_Addr);
+    LOG(L"Patch Name: %s\n", Patch_Name);
+    LOG(L"Patch length: %llu bytes\n", Patch_Size);
+    LOG(L"Patch address: 0x%016llx\n", Patch_Addr);
+    if (Patch_Function_Target)
+        LOG(L"Patch Function Target: 0x%016llx\n", Patch_Function_Target);
 }
+
+#define MEM_TYPE 0x25 // TODO: Check this heap type
 
 uint64_t Memory_PushAllocatorReturnAddr = 0;
-void __declspec(naked) Memory_PushAllocator_CC()
+void __attribute__((naked)) Memory_PushAllocator_CC()
 {
     __asm
     {
@@ -135,7 +137,7 @@ void __declspec(naked) Memory_PushAllocator_CC()
         cmp eax, 0xa
         jnz ret_loc
         retail_memory_type :
-        mov eax, 0x25
+        mov eax, MEM_TYPE
         mov dword ptr [rcx], eax
         // original code
         ret_loc :
@@ -147,29 +149,66 @@ void __declspec(naked) Memory_PushAllocator_CC()
     }
 }
 
-#define MEM_TYPE 0x25
-// 1.02
-// Game addresses
-const uint64_t ConsoleOutput_Addr = 0x3bf745;
-const uint64_t m_onDisc_DevMenu = 0x3b7692;
-const uint64_t Memory_isDebugMemoryAval = 0x3a1584;
-const uint64_t Memory_PushAllocatorJMPAddr = 0x3bf648;
-const uint64_t DebugDrawStaticContext = 0x4c7fc0;
-const uint64_t MaterialDebug_Heap = 0x5090f9;
-const uint64_t Rendering_Heap = 0x571a3c;
-const uint64_t ParticlesMenu = 0x5ded50;
-const uint64_t CreateGameplayMenu_Heap = 0x12aa26b;
-const uint64_t UpdateSelectRegionByNameMenu_Heap = 0x2561b8c;
-const uint64_t UpdateSelectSpawner = 0x26e4aeb;
-const uint64_t MenuHeap_UsableMemorySize = 0x2be15fa; // ALLOCATION_MENU_HEAP
-const uint64_t CPUHeap_UsableMemorySize = 0x2be0fdb; // ALLOCATION_CPU_MEMORY
-const uint64_t ScriptHeap_UsableMemorySize = 0x2be181a; // ALLOCATION_SCRIPT_HEAP
-const uint64_t DevMenu_MenuSize = 0x404e56;
+const wchar_t* DevMenu_MenuSize = L"c7 41 ?? 9a 99 19 3f 49 8b d1 c6 41 ?? 00 44 89 51 ??";
+const wchar_t* ConsoleOutput = L"0f 45 ce 88 0d ?? ?? ?? ?? 48 8d ?? ?? ?? ?? ?? e8 ?? ?? ?? ??";
+const wchar_t* m_onDisc_DevMenu = L"8a 8f ?? ?? ?? ?? 84 c9 0f 94 c2 84 c9 0f 95 c1";
+// thanks to infogram for offsets
+const wchar_t* Memory_isDebugMemoryAval = L"32 c0 c3 cc 4c 8b dc 49 89 5b 10 49 89 6b 18 49 89 73 20 57 41 56 41 57 48 83 ec 30 48 8d 69 30 45 8b f0 4c 8b fa 48 8d 05 ?? ?? ?? ??";
+const wchar_t* Memory_PushAllocatorJMPAddr = L"48 8d 4c 24 30 89 44 24 30 4d 8b f9 45 8b f0 48 8b ea e8 ?? ?? ?? ??";
+const wchar_t* DebugDrawStaticContext = L"e8 ?? ?? ?? ?? 33 d2 33 c9 e8 ?? ?? ?? ?? 80 b8 3d 3f 00 00 00 75 ??";
+const wchar_t* MaterialDebug_Heap = L"c7 44 24 ?? 10 00 00 00 b9 18 00 00 00 48 8d 54 24 ?? e8 ?? ?? ?? ??";
+const wchar_t* ParticlesMenu = L"40 56 48 83 ec 30 48 8b f1 33 c9 e8 ?? ?? ?? ?? 84 c0 0f 84 ?? ?? ?? ?? 48 89 5c 24 40 4c 8d 0d ?? ?? ?? ??";
+const wchar_t* UpdateSelectRegionByNameMenu_Heap = L"b9 08 40 02 00 c7 44 24 ?? 0a 00 00 00 48 8d 54 24 ??";
+const wchar_t* UpdateSelectSpawner = L"c7 44 24 ?? 0a 00 00 00 48 8d 54 24 ?? 88 5c 24 ?? b9 08 20 04 00 e8 ?? ?? ?? ??";
+const wchar_t* Memory_ValidateContext = L"cc 48 85 db 0f 84 ?? ?? ?? ?? 33 c9 ff d3 90 e9 ?? ?? ?? ?? 48 8b 43 08 49 39 47 08 75 ?? 48 8b 43 10 49 39 47 10 0f 84 ?? ?? ?? ??";
+const wchar_t* MenuHeap_UsableMemorySize = L"80 00 00 00 05 00 00 00 00 00 10 00 00 00 00 00 00 40 00 00"; // ALLOCATION_MENU_HEAP
+const wchar_t* ScriptHeap_UsableMemorySize = L"5e 00 00 00 05 00 00 00 00 00 12 00 00 00 00 00 00 40 00 00"; // ALLOCATION_SCRIPT_HEAP
+const wchar_t* CPUHeap_UsableMemorySize = L"03 00 00 00 02 00 00 00 00 00 e0 33 00 00 00 00 00 00 10 00"; // ALLOCATION_CPU_MEMORY
 // inline asserts
-const uint64_t Memory_ValidateContext = 0x117ABB3;
-const uint64_t Assert_UpdateSelectRegionByNameMenu = 0x2561b2f;
-const uint64_t Assert_UpdateSelectIgcByNameMenu = 0x25663d0;
-const uint64_t Assert_UpdateSelectSpawnerByNameMenu = 0x26e4a97;
+// based on source lines
+const wchar_t* Assert_UpdateSelectRegionByNameMenu = L"cc 48 85 db 74 ?? 33 c9 ff d3 48 8d 05 ?? ?? ?? ?? c7 44 24 ?? 10 00 00 00 41 b9 8e 00 00 00";
+const wchar_t* Assert_UpdateSelectIgcByNameMenu = L"cc 48 85 db 74 ?? 33 c9 ff d3 48 8d 05 ?? ?? ?? ?? bb 10 00 00 00 41 b9 84 03 00 00";
+const wchar_t* Assert_UpdateSelectSpawnerByNameMenu = L"cc 48 85 db 74 ?? 33 c9 ff d3 48 8d 05 ?? ?? ?? ?? c7 44 24 ?? 10 00 00 00 41 b9 a3 00 00 00";
+
+void LogPatchFailed(const wchar_t* Patch_Name, const wchar_t* Patch_Pattern)
+{
+    LOG(L"%s Pattern Scan Failed. Please adjust your scan patterns and try again\n", Patch_Name);
+    LOG(L"Pattern %s\n", Patch_Pattern);
+}
+
+void WritePatchPattern(const wchar_t* Patch_Pattern, const unsigned char* Patch_Bytes, size_t Patch_Size, const wchar_t* Patch_Name, uint64_t Patch_Offset)
+{
+    uint8_t* Address_Result = Memory::PatternScanW(baseModule, Patch_Pattern);
+    uint64_t Patch_Address = 0;
+    if (Address_Result)
+    {
+        Patch_Address = (uintptr_t)Address_Result + Patch_Offset;
+        Memory::PatchBytes(Patch_Address, Patch_Bytes, Patch_Size);
+        ShowPatchInfo(Patch_Size, Patch_Address, Patch_Name, 0);
+    }
+    else
+    {
+        LogPatchFailed(Patch_Name, Patch_Pattern);
+    }
+}
+
+void WritePatchPattern_Hook(const wchar_t* Patch_Pattern, size_t Patch_Size, const wchar_t* Patch_Name, uint64_t Patch_Offset, uint64_t Hook_Size, void* Function_Target, uint64_t* Return_Address)
+{
+    uint8_t* Address_Result = nullptr;
+    Address_Result = Memory::PatternScanW(baseModule, Patch_Pattern);
+    uint64_t Patch_Address = 0;
+    if (Address_Result)
+    {
+        Patch_Address = (uintptr_t)Address_Result + Patch_Offset;
+        *Return_Address = Patch_Address + Hook_Size;
+        Memory::DetourFunction64((void*)(Patch_Address), Function_Target, Hook_Size);
+        ShowPatchInfo(Patch_Size, Patch_Address, Patch_Name, uintptr_t(Function_Target));
+    }
+    else
+    {
+        LogPatchFailed(Patch_Name, Patch_Pattern);
+    }
+}
 
 void ApplyDebugPatches(void)
 {
@@ -181,45 +220,41 @@ void ApplyDebugPatches(void)
         else if (fDebugMenuSize < 0.4)
             fDebugMenuSize = 0.4;
         unsigned char bytes_array_4[4] = { 0 };
-        const unsigned char mov_ecx_0[] = { 0xb9, 0x00, 0x00, 0x00, 0x00, 0x90 };
         memcpy((void*)bytes_array_4, &fDebugMenuSize, sizeof(bytes_array_4));
-        Memory::PatchBytes(base_addr + DevMenu_MenuSize, bytes_array_4, sizeof(bytes_array_4));
-        Memory::PatchBytes(base_addr + m_onDisc_DevMenu, mov_ecx_0, sizeof(mov_ecx_0));
+        WritePatchPattern(DevMenu_MenuSize, bytes_array_4, sizeof(bytes_array_4), wstr(fDebugMenuSize), 3);
         memset(bytes_array_4, 0, sizeof(bytes_array_4));
+        const unsigned char mov_ecx_0[] = { 0xb9, 0x00, 0x00, 0x00, 0x00, 0x90 };
+        WritePatchPattern(m_onDisc_DevMenu, mov_ecx_0, sizeof(mov_ecx_0), wstr(m_onDisc_DevMenu), 0);
     }
     if (bShowDebugConsole)
     {
         const unsigned char mov_cl_1_nop[] = { 0xb1, 0x01, 0x90 };
-        Memory::PatchBytes(base_addr + ConsoleOutput_Addr, mov_cl_1_nop, sizeof(mov_cl_1_nop));
+        WritePatchPattern(ConsoleOutput, mov_cl_1_nop, sizeof(mov_cl_1_nop), wstr(ConsoleOutput), 0);
     }
     if (bExtendedDebugMenu)
     {
-        const unsigned char mov_eax_0[] = { 0xb8, 0x00, 0x00, 0x00, 0x00 };
         const unsigned char nop5x[] = { 0x90, 0x90, 0x90, 0x90, 0x90 };
+        const unsigned char ret_1_al[] = { 0xb0, 0x01, 0xc3 };
         const unsigned char nop1x[] = { 0x90 };
         const unsigned char ret_0[] = { 0x31, 0xc0, 0xc3 };
-        const unsigned char ret_1_al[] = { 0xb0, 0x01, 0xc3 };
         const unsigned char heap_type[] = { MEM_TYPE };
         const unsigned char menu_mem_size[] = { 0x84 };
         const unsigned char script_mem_size[] = { 0x80 };
         const unsigned char cpu_mem_size[] = { 0x40 };
-        Memory_PushAllocatorReturnAddr = base_addr + Memory_PushAllocatorJMPAddr + 15;
-        Memory::DetourFunction64((void*)(base_addr + Memory_PushAllocatorJMPAddr), Memory_PushAllocator_CC, 15);
-        Memory::PatchBytes(base_addr + Memory_isDebugMemoryAval, ret_1_al, sizeof(ret_1_al)); // We have debug memory (not really)
-        Memory::PatchBytes(base_addr + DebugDrawStaticContext, nop5x, sizeof(nop5x)); // DebugDrawStaticContext
-        Memory::PatchBytes(base_addr + MaterialDebug_Heap, heap_type, sizeof(heap_type)); // Material Debug heap override
-        Memory::PatchBytes(base_addr + ParticlesMenu, ret_0, sizeof(ret_0)); // Particles menu
-        Memory::PatchBytes(base_addr + CreateGameplayMenu_Heap, heap_type, sizeof(heap_type)); // Gameplay
-        Memory::PatchBytes(base_addr + UpdateSelectRegionByNameMenu_Heap, heap_type, sizeof(heap_type)); // Select region heap override
-        Memory::PatchBytes(base_addr + UpdateSelectSpawner, heap_type, sizeof(heap_type)); // UpdateSelectSpawner heap override
-        Memory::PatchBytes(base_addr + MenuHeap_UsableMemorySize, menu_mem_size, sizeof(menu_mem_size)); // Increase memory allocation size
-        Memory::PatchBytes(base_addr + ScriptHeap_UsableMemorySize, script_mem_size, sizeof(script_mem_size)); // Script memory
-        Memory::PatchBytes(base_addr + CPUHeap_UsableMemorySize, cpu_mem_size, sizeof(cpu_mem_size)); // CPU memory
-        // inline asserts
-        Memory::PatchBytes(base_addr + Memory_ValidateContext, nop1x, sizeof(nop1x)); // Memory::ValidateContext
-        Memory::PatchBytes(base_addr + Assert_UpdateSelectRegionByNameMenu, nop1x, sizeof(nop1x)); // Select region
-        Memory::PatchBytes(base_addr + Assert_UpdateSelectIgcByNameMenu, nop1x, sizeof(nop1x)); // Select igc
-        Memory::PatchBytes(base_addr + Assert_UpdateSelectSpawnerByNameMenu, nop1x, sizeof(nop1x)); // Select igc
+        WritePatchPattern_Hook(Memory_PushAllocatorJMPAddr, 15, wstr(Memory_PushAllocatorJMPAddr), 0, 15, &Memory_PushAllocator_CC, &Memory_PushAllocatorReturnAddr);
+        WritePatchPattern(Memory_isDebugMemoryAval, ret_1_al, sizeof(ret_1_al), wstr(Memory_isDebugMemoryAval), 0);
+        WritePatchPattern(DebugDrawStaticContext, nop5x, sizeof(nop5x), wstr(DebugDrawStaticContext), 0);
+        WritePatchPattern(MaterialDebug_Heap, heap_type, sizeof(heap_type), wstr(MaterialDebug_Heap), 4);
+        WritePatchPattern(ParticlesMenu, ret_0, sizeof(ret_0), wstr(ParticlesMenu), 0);
+        WritePatchPattern(UpdateSelectRegionByNameMenu_Heap, heap_type, sizeof(heap_type), wstr(UpdateSelectRegionByNameMenu_Heap), 9);
+        WritePatchPattern(UpdateSelectSpawner, heap_type, sizeof(heap_type), wstr(UpdateSelectSpawner), 4);
+        WritePatchPattern(MenuHeap_UsableMemorySize, menu_mem_size, sizeof(menu_mem_size), wstr(MenuHeap_UsableMemorySize), 10);
+        WritePatchPattern(ScriptHeap_UsableMemorySize, script_mem_size, sizeof(script_mem_size), wstr(ScriptHeap_UsableMemorySize), 10);
+        WritePatchPattern(CPUHeap_UsableMemorySize, cpu_mem_size, sizeof(cpu_mem_size), wstr(CPUHeap_UsableMemorySize), 11);
+        WritePatchPattern(Memory_ValidateContext, nop1x, sizeof(nop1x), wstr(Memory_ValidateContext), 0);
+        WritePatchPattern(Assert_UpdateSelectRegionByNameMenu, nop1x, sizeof(nop1x), wstr(Assert_UpdateSelectRegionByNameMenu), 0);
+        WritePatchPattern(Assert_UpdateSelectIgcByNameMenu, nop1x, sizeof(nop1x), wstr(Assert_UpdateSelectIgcByNameMenu), 0);
+        WritePatchPattern(Assert_UpdateSelectSpawnerByNameMenu, nop1x, sizeof(nop1x), wstr(Assert_UpdateSelectSpawnerByNameMenu), 0);
     }
 }
 
@@ -233,7 +268,7 @@ void __stdcall Main()
     LoggingInit();
     ReadConfig();
     ApplyDebugPatches();
-    LOG(L"Shutting down " wstr(fp_log) " file handle.");
+    LOG(L"Shutting down " wstr(fp_log) " file handle.\n");
     fclose(fp_log);
 }
 
