40bf79e2f35503cf445f3240c7f6890f5ecd7bfa
TL431 <tl43one@gmail.com>
2023-07-15 13:40:32 +0200

    Planet of Lana TAA fix
    Rename dinput8.dll to d3d11.dll and copy d3d11.ini in the game folder.
    The setting DontLoadFromDllMain=0 in d3d11.ini is mandatory to make the game load the ASI patch without crashing.

---
+++ source/PlanetOfLana.NoTAA/dllmain.cpp
@@ -0,0 +1,93 @@
+#include "stdafx.h"
+#include "helper.hpp"
+#include "memory.hpp"
+#include "git_ver.h"
+
+HMODULE baseModule = GetModuleHandle(L"GameAssembly.dll");
+
+#define wstr(s) L#s
+#define wxstr(s) wstr(s)
+#define _PROJECT_NAME L"PlanetOfLana.NoTAA"
+#define _PROJECT_LOG_PATH _PROJECT_NAME L".log"
+
+wchar_t exePath[_MAX_PATH] = { 0 };
+
+// INI Variables
+bool bDisableTAA;
+
+void ReadConfig(void)
+{
+    inipp::Ini<wchar_t> ini;
+    // Get game name and exe path
+    LOG(_PROJECT_NAME " Built: " __TIME__ " @ " __DATE__ "\n");
+    LOG(L"" GIT_COMMIT "\n");
+    LOG(L"" GIT_VER "\n");
+    LOG(L"" GIT_NUM "\n");
+    LOG(L"Game Name: %s\n", Memory::GetVersionProductName().c_str());
+    LOG(L"Game Path: %s\n", exePath);
+
+    // Initialize config
+    // UE4 games use launchers so config path is relative to launcher
+    std::wstring config_path = _PROJECT_NAME L".ini";
+    std::wifstream iniFile(config_path);
+    if (!iniFile)
+    {
+        // no ini, lets generate one.
+        LOG(L"Failed to load config file.\n");
+        std::wstring ini_defaults = L"[Settings]\n"
+                                    wstr(bDisableTAA)" = true\n";
+        std::wofstream iniFile(config_path);
+        iniFile << ini_defaults;
+        bDisableTAA = true;
+        LOG(L"Created default config file.\n");
+    }
+    else
+    {
+        ini.parse(iniFile);
+        inipp::get_value(ini.sections[L"Settings"], wstr(bDisableTAA), bDisableTAA);
+    }
+
+    // Log config parse
+    LOG(L"%s: %s (%i)\n", wstr(bDisableTAA), GetBoolStr(bDisableTAA) , bDisableTAA);
+}
+
+void DisableTAA(void)
+{
+    const unsigned char patch[] = { 0x33, 0xC0, 0x89, 0x47, 0x28, 0x90, 0x90, 0x90 };
+    WritePatchPattern(L"8B 47 28 83 F8 01 74 22 83 F8 02", patch, sizeof(patch), L"Disable TAA", 0);
+}
+
+DWORD __stdcall Main(void*)
+{
+    bLoggingEnabled = false;
+    bDisableTAA = false;
+    wchar_t LogPath[_MAX_PATH] = { 0 };
+    wcscpy_s(exePath, _countof(exePath), GetRunningPath(exePath));
+    _snwprintf_s(LogPath, _countof(LogPath), _TRUNCATE, L"%s\\%s", exePath, _PROJECT_LOG_PATH);
+    LoggingInit(_PROJECT_NAME, LogPath);
+    ReadConfig();
+    if (bDisableTAA)
+        DisableTAA();
+    LOG(L"Shutting down " wstr(fp_log) " file handle.\n");
+    fclose(fp_log);
+    return true;
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    {
+        CreateThread(NULL, 0, Main, 0, NULL, 0);
+    }
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
