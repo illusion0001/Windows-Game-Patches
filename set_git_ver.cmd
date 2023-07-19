72a7ac72c9b0290a548077c2bd9f6865ae76838d
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2023-07-18 22:50:16 -0500

    CI: Try to add extra ini files

--- set_git_ver.cmd
+++ set_git_ver.cmd
@@ -1,8 +1,15 @@
+@echo off
+
 for /f %%a in ('git rev-parse HEAD') do set "GIT_COMMIT=%%a"
 for /f %%a in ('git branch --show-current') do set "GIT_VER=%%a"
 for /f %%a in ('git rev-list HEAD --count') do set "GIT_NUM=%%a"
 
-echo #define GIT_COMMIT "Git Commit: %GIT_COMMIT%" > ..\..\include\git_ver.h
-echo #define GIT_VER "Git Branch: %GIT_VER%" >> ..\..\include\git_ver.h
-echo #define GIT_NUM "Git Version: %GIT_NUM%" >> ..\..\include\git_ver.h
-type ..\..\include\git_ver.h
+set "GIT_VER_FILE=.\include\git_ver.h"
+if not exist "%GIT_VER_FILE%" (
+    set "GIT_VER_FILE=..\..\include\git_ver.h"
+)
+
+echo #define GIT_COMMIT "Git Commit: %GIT_COMMIT%" > "%GIT_VER_FILE%"
+echo #define GIT_VER "Git Branch: %GIT_VER%" >> "%GIT_VER_FILE%"
+echo #define GIT_NUM "Git Version: %GIT_NUM%" >> "%GIT_VER_FILE%"
+type "%GIT_VER_FILE%"
