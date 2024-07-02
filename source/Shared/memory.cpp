b54b680af57d9c601d050d688a42455d030a192c
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2024-07-02 01:52:17 -0500

    DetourFunction64: remove not useful trampoline allocation (never used)

--- source/Shared/memory.cpp
+++ source/Shared/memory.cpp
@@ -54,24 +54,20 @@ namespace Memory
 
     void* DetourFunction64(void* pSource, void* pDestination, DWORD dwLen)
     {
-        DWORD MinLen = 14;
+        constexpr DWORD MinLen = 14;
+
+        if (dwLen < MinLen)
+        {
+            return nullptr;
+        }
 
         BYTE stub[] = {
         0xFF, 0x25, 0x00, 0x00, 0x00, 0x00, // jmp qword ptr [$+6]
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 // ptr
         };
 
-        void* pTrampoline = VirtualAlloc(0, dwLen + sizeof(stub), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-
         DWORD dwOld = 0;
         VirtualProtect(pSource, dwLen, PAGE_EXECUTE_READWRITE, &dwOld);
-
-        DWORD64 retto = (DWORD64)pSource + dwLen;
-
-        // trampoline
-        memcpy(stub + 6, &retto, 8);
-        memcpy((void*)((DWORD_PTR)pTrampoline), pSource, dwLen);
-        memcpy((void*)((DWORD_PTR)pTrampoline + dwLen), stub, sizeof(stub));
 
         // orig
         memcpy(stub + 6, &pDestination, 8);
@@ -83,7 +79,7 @@ namespace Memory
         }
 
         VirtualProtect(pSource, dwLen, dwOld, &dwOld);
-        return (void*)((DWORD_PTR)pTrampoline);
+        return pDestination;
     }
 
     HMODULE GetThisDllHandle()
