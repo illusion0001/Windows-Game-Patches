f990bd7e2cc5ab0fddf2f1c9ea0d6caf66341e8e
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2025-01-22 08:39:03 -0600

    Add HDE64 and update memory and helper friends

--- include/helper.hpp
+++ include/helper.hpp
@@ -29,6 +29,7 @@ void WritePatchAddressFloat64(uint64_t Patch_Address, const double* Patch_Bytes,
 wchar_t* GetRunningPath(wchar_t* output);
 wchar_t* GetModuleName(wchar_t* output);
 uintptr_t FindAndPrintPatternW(const wchar_t* Patch_Pattern, const wchar_t* Pattern_Name, size_t offset = 0);
+uintptr_t FindAndPrintPatternW(const HMODULE Module, const wchar_t* Patch_Pattern, const wchar_t* Pattern_Name, size_t offset = 0);
 void Make32to64Call(void* source_target, void* second_jmp, void* target_jmp, uint32_t source_size, const wchar_t* source_name, const wchar_t* second_jmp_name, const wchar_t* target_jmp_name);
 void Make32to64Hook(void* source_target, void* second_jmp, void* target_jmp, uint32_t source_size, const wchar_t* source_name, const wchar_t* second_jmp_name, const wchar_t* target_jmp_name);
 void Make32Hook(void* source_target, void* target_jmp, uint32_t source_size, const wchar_t* source_name, const wchar_t* target_jmp_name);
@@ -58,6 +59,25 @@ constexpr StringId64 ToStringId64(const char* str)
 void SendInputWrapper(WORD inputKey);
 void CheckCooldown(bool isBenchmarking, size_t BenchmarkIndex);
 void CheckScriptFile();
+
+#pragma GCC diagnostic ignored "-Wunused-function"
+
+static void ShowConsole1()
+{
+    AllocConsole();
+    SetConsoleTitle(L"" PROJECT_NAME " - Debug Console");
+    freopen_s(reinterpret_cast<FILE**>(stdin), "conin$", "r", stdin);
+    freopen_s(reinterpret_cast<FILE**>(stdout), "conout$", "w", stdout);
+    freopen_s(reinterpret_cast<FILE**>(stderr), "conout$", "w", stderr);
+    ShowWindow(GetConsoleWindow(), SW_SHOW);
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD dwMode = 0;
+    GetConsoleMode(hConsole, &dwMode);
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    SetConsoleMode(hConsole, dwMode);
+}
+
+#pragma GCC diagnostic warning "-Wunused-function"
 
 #pragma GCC diagnostic ignored "-Wmicrosoft-string-literal-from-predefined"
 // translates to `"foo():1234: "`
