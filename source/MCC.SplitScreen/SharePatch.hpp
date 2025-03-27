e462cb37d78ee57a32b7ee520b34f176df3ba970
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2025-03-27 21:42:51 +1300

    MCC: Add Split Screen patch

---
+++ source/MCC.SplitScreen/SharePatch.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <stdint.h>
+
+static const uint8_t jmp[] = { 0x48, 0xe9 };
---
+++ source/MCC.SplitScreen/halo3.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <Windows.h>
+#include <stdbool.h>
+
+void PatchHalo3(HMODULE Module, bool isOdst = false);
